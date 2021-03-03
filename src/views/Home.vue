db56ccf5b80ad73b3caa33bdc953114fe6f7b41a
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-03-03 10:04:43 -0300

    axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: 'Home',
-  components: {}
-}
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
