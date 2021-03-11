def30ea8961f6a6e685242943326a0e9ad287bd0
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-03-11 10:14:10 -0300

    pagination

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
 </template>
 
 <script>
-import TheHeader from "@/components/TheHeader.vue";
-import TheFooter from "@/components/TheFooter.vue";
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
 
 export default {
   components: {
     TheHeader,
-    TheFooter,
-  },
-};
+    TheFooter
+  }
+}
 </script>
 
 <style>
@@ -40,9 +40,9 @@ ul {
 }
 
 body {
-  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
   color: #234;
-  background: url("./assets/pattern.svg") repeat top;
+  background: url('./assets/pattern.svg') repeat top;
 }
 
 a {
@@ -66,7 +66,7 @@ img {
   box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
   transition: all 0.3s;
   border: none;
-  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
   cursor: pointer;
 }
 
@@ -83,5 +83,26 @@ img {
 
 #main {
   flex: 1;
+}
+
+input,
+textarea {
+  border-radius: 4px;
+  border: 1px solid white;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
+  font-size: 1rem;
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  margin-bottom: 15px;
+}
+
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87f;
 }
 </style>
--- src/services.js
+++ src/services.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
-});
+  baseURL: 'http://localhost:3000'
+})
 
 export const api = {
-  get(endpoint) {
-    return axiosInstance.get(endpoint);
+  get (endpoint) {
+    return axiosInstance.get(endpoint)
   },
-  post(endpoint, body) {
-    return axiosInstance.post(endpoint, body);
-  },
-};
+  post (endpoint, body) {
+    return axiosInstance.post(endpoint, body)
+  }
+}
